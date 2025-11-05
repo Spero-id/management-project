a573e07429c72b95fa040433dfc5df09ff7accad
Hajid Al Akhtar <hajidalakhtar@gmail.com>
2025-11-05 09:15:38 +0700

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +18,12 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+    public function boot(): void{
+        // Source - https://stackoverflow.com/questions/35827062/how-to-force-laravel-project-to-use-https-for-all-routes
+        // Posted by Amitesh Bharti
+        // Retrieved 11/5/2025, License - CC-BY-SA 4.0
+
+        URL::forceScheme('https');
+
     }
 }
